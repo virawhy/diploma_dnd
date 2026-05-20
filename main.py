c34e4377c0e21f3261c9d1dee32c56f8619e2506
virawhy <6334980@stud.nau.edu.ua>
2026-05-20 17:48:49 +0300

    edit sign up | log in

--- main.py
+++ main.py
@@ -1837,8 +1837,12 @@ def auth():
 
         user = User.query.filter_by(email=email).first()
 
-        if not user or not check_password_hash(user.password, password):
-            flash('Логін або пароль введено невірно')
+        if not user:
+            flash('Акаунт із такою e-mail адресою не знайдено. Перевірте адресу або зареєструйтесь.', 'account_missing')
+            return redirect(url_for('auth'))
+
+        if not check_password_hash(user.password, password):
+            flash('Пароль введено невірно.', 'error')
             return redirect(url_for('auth'))
         login_user(user)
         return redirect(url_for("profile"))
@@ -1856,15 +1860,19 @@ def logout():
 @app.route("/registration.html", methods=("POST", "GET"))
 def registration():
     if request.method == "POST":
+        username = request.form['username'].strip()
         password = request.form['password']
         confirm_password = request.form['confirm_password']
         if password != confirm_password:
-            flash("Паролі не збігаються")
+            flash("Паролі не збігаються", "error")
+            return redirect(url_for("registration"))
+        if User.query.filter_by(username=username).first():
+            flash("Профіль із таким нікнеймом вже існує. Оберіть інший нікнейм.", "username_taken")
             return redirect(url_for("registration"))
         else:
             try:
                 hash = generate_password_hash(password)
-                new_user = User(username=request.form['username'], email=request.form['email'],
+                new_user = User(username=username, email=request.form['email'],
                                 password=hash)
                 db.session.add(new_user)
                 db.session.flush()
@@ -1873,7 +1881,7 @@ def registration():
             except Exception as e:
                 db.session.rollback()
                 current_app.logger.exception("Error adding user to database: %s", e)
-                flash("Не вдалося створити акаунт")
+                flash("Не вдалося створити акаунт", "error")
     return render_template("registration.html")
 
 
